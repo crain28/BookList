ad7c65779b115d2e48f168306c48d38e5b58de44
Cole Crain <54562876+crain28@users.noreply.github.com>
2020-09-22 20:51:46 -0400

    Add files via upload

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using BookList.Models;
+
+namespace BookList.Controllers
+{
+    public class HomeController : Controller
+    {
+        private BookContext context { get; set; }
+
+        public HomeController(BookContext ctx)
+        {
+            context = ctx;
+        }
+
+          public IActionResult Index()
+        {
+
+            var books = context.Books.Include(m=>m.Genre)
+                .OrderBy(m => m.Name).ToList();
+            return View(books);
+        }
+
+    }
+}
